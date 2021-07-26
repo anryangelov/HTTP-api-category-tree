0fe46e252a0049e82aff3f106e3a9ded64a868f0
Angel Angelov <angel.angelov@adata.pro>
2021-07-26 05:46:32 +0300

    Add logic for finding longest rabbit hole

---
+++ category_tree/category_tree_app/management/commands/get_longest_rabbit_hole.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from category_tree_app.models import Category
+from category_tree_app.utils import get_longest_rabbit_holes
+
+
+class Command(BaseCommand):
+    help = 'Get longest rabbit hole'
+
+    def handle(self, *args, **options):
+        result = set(Category.similarities.through.objects.values_list(
+            'from_category_id', 'to_category_id'
+        ))
+        paths = get_longest_rabbit_holes(result)
+
+        for category_ids in paths:
+            categories = Category.objects.filter(pk__in=category_ids)
+            categories = {c.pk: c.name for c in categories}
+            path = [categories[c_id] for c_id in category_ids]
+            self.stdout.write(self.style.SUCCESS(path))
--- category_tree/category_tree_app/utils.py
+++ category_tree/category_tree_app/utils.py
@@ -58,3 +58,58 @@ def get_islands_categories(islands_vertexes):
             categories.update(vertex)
         islands.append(categories)
     return islands
+
+
+def get_graph(island_vertexes):
+    graph = defaultdict(list)
+    for p1, p2 in sorted(list(island_vertexes)):
+        graph[p1].append(p2)
+    return graph
+
+
+def all_shortest_paths_for_category_in_island(graph, category_id):
+    '''essensally bread first search'''
+    paths = []
+    path = (category_id,)
+    visited = set([category_id])
+    queue = deque()
+    queue.append((path, graph.get(category_id, [])))
+    while queue:
+        path, category_ids = queue.popleft()
+        category_ids = [c for c in category_ids if c not in visited]
+        if not category_ids:
+            paths.append(path)
+        for category_id in category_ids:
+            new_path = path + (category_id,)
+            #new_path.append(category_id)
+            visited.add(category_id)
+            queue.append((new_path, graph.get(category_id, [])))
+
+    return paths
+
+
+def get_longest_lists(lists):
+    max_len = len(max(lists, key=len))
+    return [l for l in lists if len(l) == max_len]
+
+
+def remove_reverse_paths(paths):
+    result = set()
+    for path in paths:
+        if path[0] > path[-1]:
+            path = tuple(reversed(path))
+        result.add(path)
+    return result
+
+
+def get_longest_rabbit_holes(similarities):
+    islands_longest_paths = []
+    islands_vertexes = get_islands_vertexes(similarities)
+    for island_vertexes in islands_vertexes:
+        graph = get_graph(island_vertexes)
+        for category_id in graph.keys():
+            paths = all_shortest_paths_for_category_in_island(graph, category_id)
+            longest_paths = get_longest_lists(paths)
+            islands_longest_paths.extend(longest_paths)
+
+    return remove_reverse_paths(get_longest_lists(islands_longest_paths))
